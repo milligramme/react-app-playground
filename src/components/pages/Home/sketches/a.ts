93364339de2c873d42504e7a3a765f562bc5f148
milligramme <milligramme.cc@gmail.com>
2021-07-18 11:05:41 +0900

    Clear sketch on click

--- src/components/pages/Home/sketches/a.ts
+++ src/components/pages/Home/sketches/a.ts
@@ -14,6 +14,11 @@ const sketch = (p5: p5) => {
     p5.ellipse(p5.width * Math.random(), p5.height * Math.random(), 12, 12);
     p5.pop();
   };
+
+  p5.mouseClicked = () => {
+    p5.fill(255);
+    p5.rect(0, 0, p5.width, p5.height);
+  };
 };
 
 export default sketch;
