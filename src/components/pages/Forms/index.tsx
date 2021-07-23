1c7b3756b0780a6fe86e77c1f6e702a8e9d24988
milligramme <milligramme.cc@gmail.com>
2021-07-23 14:01:22 +0900

    Update form

--- src/components/pages/Forms/index.tsx
+++ src/components/pages/Forms/index.tsx
@@ -1,17 +1,44 @@
+import { useEffect } from "react";
 import { Form, Field } from "react-final-form";
 
-import { Box, Typography, TextField, Button } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  TextField,
+  Typography
+} from "@material-ui/core";
 
 import MainTmpl from "components/templates/MainTmpl";
 import { sleep } from "lib/helpers";
+import { useListUsersApi } from "utils/api/hooks/useListUsersApi";
+import { User } from "types";
 
 interface FormValue {
   name: string;
+  user: number;
 }
 
 const Forms: React.FunctionComponent = () => {
   const required = (value: FormValue) =>
     value === undefined ? "required" : undefined;
+
+  const { data: users, isLoading, isError, isSuccess } = useListUsersApi();
+
+  useEffect(() => {
+    if (isError) {
+      console.log("error occured");
+    }
+  }, [isError]);
+
+  if (isLoading) {
+    <MainTmpl>loading...</MainTmpl>;
+  }
+
   return (
     <MainTmpl>
       <Box>
@@ -22,32 +49,59 @@ const Forms: React.FunctionComponent = () => {
             await sleep(1000);
             console.log(JSON.stringify({ values }, null, "  "));
           }}
-          subscription={{ submitting: true }}
+          subscription={{ submitting: true, values: true }}
         >
-          {({ handleSubmit, submitting }) => (
+          {({ handleSubmit, submitting, values }) => (
             <>
-              <Field
-                name="name"
-                validate={required}
-                subscription={{ value: true, error: true, touched: true }}
-              >
-                {({ input, meta }) => (
-                  <TextField
-                    {...input}
-                    error={!!meta.error && meta.touched}
-                    variant="outlined"
-                    label="name"
-                    InputLabelProps={{
-                      shrink: true
-                    }}
-                    FormHelperTextProps={{
-                      variant: "outlined",
-                      error: !!meta.error
-                    }}
-                    helperText={meta.error && meta.touched ? meta.error : ""}
-                  />
-                )}
-              </Field>
+              <pre>{JSON.stringify({ values }, null, "  ")}</pre>
+              <Box>
+                <Field
+                  name="name"
+                  validate={required}
+                  subscription={{ value: true, error: true, touched: true }}
+                >
+                  {({ input, meta }) => (
+                    <TextField
+                      {...input}
+                      error={!!meta.error && meta.touched}
+                      variant="outlined"
+                      label="name"
+                      InputLabelProps={{
+                        shrink: true
+                      }}
+                      FormHelperTextProps={{
+                        variant: "outlined",
+                        error: !!meta.error
+                      }}
+                      helperText={meta.error && meta.touched ? meta.error : ""}
+                    />
+                  )}
+                </Field>
+              </Box>
+              <Box>
+                <Field name="user" type="radio" subscription={{ value: true }}>
+                  {({ input }) => (
+                    <FormControl>
+                      <FormLabel>Select favorite user</FormLabel>
+                      <RadioGroup
+                        value={input.value}
+                        row={false}
+                        onChange={input.onChange}
+                      >
+                        {isSuccess &&
+                          (users as User[]).map((user) => (
+                            <FormControlLabel
+                              key={user.id}
+                              value={String(user.id)}
+                              control={<Radio />}
+                              label={`${user.name} (${user.email})`}
+                            />
+                          ))}
+                      </RadioGroup>
+                    </FormControl>
+                  )}
+                </Field>
+              </Box>
               <Box>
                 <Button
                   size="large"
