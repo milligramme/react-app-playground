55e4748fc97c8163a5fc66301e2a1e9b5396b52a
milligramme <milligramme.cc@gmail.com>
2021-07-23 13:59:57 +0900

    Add axios client

---
+++ src/utils/api/index.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL
+});
+
+api.interceptors.request.use((request) => {
+  console.log("#############\nRequest: ", request);
+  return request;
+});
+
+api.interceptors.response.use(
+  (response) => {
+    console.log("#############\nResponse: ", response);
+    return Promise.resolve(response);
+  },
+  (error) => Promise.reject({ error: error.response })
+);
