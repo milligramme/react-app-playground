057bf142875c24f22a199339f8f20669c74702e3
milligramme <milligramme.cc@gmail.com>
2021-10-09 21:10:53 +0900

    Update hooks

--- src/utils/api/hooks/useListUsersApi/index.ts
+++ src/utils/api/hooks/useListUsersApi/index.ts
@@ -1,14 +1,22 @@
-import { useQuery } from "react-query";
+import { useQuery, UseQueryOptions } from "react-query";
 
 import { api } from "utils/api";
 
-export const useListUsersApi = () =>
-  useQuery("/users", async () => {
-    try {
-      const { data } = await api.get("/users");
-      return data;
-    } catch (e) {
-      console.log(e);
-      throw e;
-    }
-  });
+export const useListUsersApi = (
+  opt?: UseQueryOptions<unknown, unknown, unknown, "/users">,
+) => {
+  console.log(opt);
+  return useQuery(
+    "/users",
+    async () => {
+      try {
+        const { data } = await api.get("/users");
+        return data;
+      } catch (e) {
+        console.log(e);
+        throw e;
+      }
+    },
+    opt,
+  );
+};
